144e21583e870724f7021f0ff1579bf2280e54a9
jonathasgouv <jonathas_gouv@hotmail.com>
2020-05-01 12:41:42 -0300

    Add try-catch structure

--- content.js
+++ content.js
@@ -12,19 +12,21 @@ chrome.storage.sync.get(
 setInterval(function() {
 	url = document.location.toString();
 	if (url.includes('vk.com/topic-73721457_')) {
-		posts = document.getElementsByClassName('bp_info');
-		for (i = 0; i <= posts.length; i++) {
-			if (
-				posts[i]
-					.getElementsByClassName('bp_author_wrap')[0]
-					.getElementsByClassName('bp_author')[0]
-					.href.toString() == urlmembro.toString()
-			) {
-				posts[i].innerHTML = 'MEMBRO BLOQUEADO POR SER MUITO RUIM';
-				posts[i].style.textAlign = 'center';
-				posts[i].style.fontWeight = 'bolder';
+		try {
+			posts = document.getElementsByClassName('bp_info');
+			for (i = 0; i <= posts.length; i++) {
+				if (
+					posts[i]
+						.getElementsByClassName('bp_author_wrap')[0]
+						.getElementsByClassName('bp_author')[0]
+						.href.toString() == urlmembro.toString()
+				) {
+					posts[i].innerHTML = 'MEMBRO BLOQUEADO POR SER MUITO RUIM';
+					posts[i].style.textAlign = 'center';
+					posts[i].style.fontWeight = 'bolder';
+				}
 			}
-		}
+		} catch (error) {}
 	} else {
 	}
 }, 500);
